98782363e81c835b6c425958163fd603a8c0ccb3
danny <danny.dev8@gmail.com>
2018-01-05 00:33:44 +0900

    Add gz compress

--- restore.sh
+++ restore.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-if aws s3 cp s3://$S3_BUCKET/$MYSQL_DATABASE.sql $MYSQL_DATABASE.sql; then
-  mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $MYSQL_DATABASE.sql
+if aws s3 cp s3://$S3_BUCKET/$MYSQL_DATABASE.sql.gz $MYSQL_DATABASE.sql.gz; then
+  zcat $MYSQL_DATABASE.sql.gz | mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE
 fi
